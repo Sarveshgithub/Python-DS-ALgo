1dcaa14112690d5f9fef51248cc84f935a83c286
sarvesh574 <sarvesh.kumar@silverlinecrm.com>
2019-09-20 18:05:42 +0530

    done nestedList

--- nestedList.py
+++ nestedList.py
@@ -1,11 +1,11 @@
-# a = list()
-# for i in range(3):
-#     a.append([input(), float(input())])
-# print(a)
-student = [["sss", 1.3], ["dfff", 3.3], ["sarvesh", 5.3], ["Lokesh", 2.3], ["lll", 2.3]]
+n = int(input())
+student = list()
+for i in range(n):
+    student.append([input(), float(input())])
 
 score = list(set([student[i][1] for i in range(len(student))]))
 score.sort()
 output = [x[0] for x in student if x[1] == score[1]]
+output.sort()
 for s in output:
     print(s)
