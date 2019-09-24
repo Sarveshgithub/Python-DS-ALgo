816388a03d91cf2aaae34bda723800fef88b01ff
sarvesh574 <sarvesh.kumar@silverlinecrm.com>
2019-09-24 12:55:45 +0530

    Add AlterNate Solution

--- HackerRank/DoorMat.py
+++ HackerRank/DoorMat.py
@@ -9,3 +9,9 @@ print("WELCOME".center(m, "-"))
 for i in range(n):
     if i % 2 != 0:
         print((".|." * (n - i - 1)).center(m, "-"))
+
+print("AlterNate Solution".center(m, "-"))
+
+o = [(".|." * (2 * i + 1)).center(m, "-") for i in range(n // 2)]
+print("\n".join(o + ["WELCOME".center(m, "-")] + o[::-1]))
+
