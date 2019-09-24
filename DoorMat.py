36f2ceffb65a1e5111b276be78a010b6676e9cb7
sarvesh574 <sarvesh.kumar@silverlinecrm.com>
2019-09-24 12:15:59 +0530

    Complete DoorMat

--- DoorMat.py
+++ DoorMat.py
@@ -1,2 +1,11 @@
 # Problem
 # https://www.hackerrank.com/challenges/designer-door-mat/problem
+a = input().split()
+n, m = int(a[0]), int(a[1])
+for i in range(n):
+    if i % 2 != 0:
+        print((".|." * i).center(m, "-"))
+print("WELCOME".center(m, "-"))
+for i in range(n):
+    if i % 2 != 0:
+        print((".|." * (n - i - 1)).center(m, "-"))
