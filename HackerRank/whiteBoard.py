20afeadc4de33490712d9b797c54d998fb38bf9f
sarvesh574 <sarvesh.kumar@silverlinecrm.com>
2019-09-26 13:12:55 +0530

    added git ignore

--- HackerRank/whiteBoard.py
+++ HackerRank/whiteBoard.py
@@ -1,3 +1,16 @@
-# n, m = map(int, input().split())
-# print(n, m)
+# # n = int(input())
+# # width = "{0:b}".format(n)
+# # print(width)
 
+# m = {}
+# alpha = "a"
+# for i in range(0, 26):
+#     m[i] = alpha
+#     alpha = chr(ord(alpha) + 1)
+# print(m)
+import string
+
+alpha = string.ascii_lowercase
+print(alpha[2:3])
+s = "cbwqqwq"
+print(s[:-1])
