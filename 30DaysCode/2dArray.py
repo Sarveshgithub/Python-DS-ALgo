429e18e224bbfa0b9500a7f415df2962be13624d
sarvesh574 <sarvesh.kumar@silverlinecrm.com>
2019-10-04 23:17:31 +0530

    optimized

--- 30DaysCode/2dArray.py
+++ 30DaysCode/2dArray.py
@@ -1,13 +1,9 @@
 array = [input().split(" ") for i in range(6)]
 hourglass = []
 
-for i in range(6):
-    if i == 4:
-        break
-    for j in range(6):
+for i in range(4):
+    for j in range(3):
         t = array[i][j : j + 3] + array[i + 1][j + 1 : j + 2] + array[i + 2][j : j + 3]
         hourglass.append(sum(map(int, t)))
-        if j == 3:
-            break
 hourglass.sort()
 print(hourglass[::-1][0])
