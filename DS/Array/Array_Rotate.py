aabf1e54fa4e93f222b7a1b05bc6e0d1b913e557
sarvesh574 <sarvesh.kumar@silverlinecrm.com>
2019-10-06 02:15:18 +0530

    done

--- DS/Array/Array_Rotate.py
+++ DS/Array/Array_Rotate.py
@@ -1,12 +1,34 @@
 # https://www.geeksforgeeks.org/array-rotation/
 # t = 1
-arr = [1, 2, 3, 4]
 
-size = len(arr)
-temp = arr[size - 1]
-# lastElement = arr[size - 1]
-for i in range(0, size - 1):
-    arr[size - 1 - i] = arr[size - 1 - i - 1]
-arr[0] = temp
-print(arr)
+
+def rightRotaion(arr):
+    size = len(arr)
+    temp = arr[size - 1]
+    for i in range(0, size - 1):
+        arr[size - 1 - i] = arr[size - 1 - i - 1]
+    arr[0] = temp
+    return arr
+
+
+output = []
+n = int(input())
+for _ in range(n):
+    input()
+    a = [int(i) for i in input().split(" ")]
+    s = len(a)
+    tempArr = a
+    count = 0
+    for i in reversed(range(s)):
+        tempArr = rightRotaion(tempArr)
+        if i - count > -1:
+            del tempArr[i - count]
+        else:
+            del tempArr[0]
+        count += 1
+        if len(tempArr) == 1:
+            break
+    output.append(str(tempArr[0]))
+
+print("\n".join(output))
 
