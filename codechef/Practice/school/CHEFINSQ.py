c8258c348a791fcda59d81068eff5de6c9090837
sarvesh574 <sarvesh.kumar@silverlinecrm.com>
2019-10-07 19:47:23 +0530

    Incomplete

--- codechef/Practice/school/CHEFINSQ.py
+++ codechef/Practice/school/CHEFINSQ.py
@@ -1,17 +1,17 @@
 # https://www.codechef.com/problems/CHEFINSQ
 
-l = list(map(int, input().split()))
-subset = {}
-for i in range(len(l)):
-    for j in range(i, len(l) - 1):
-        s = l[i] + l[j + 1]
-        if s in subset:
-            subset.get(s).append((l[i], l[j + 1]))
-        else:
-            subset[s] = [(l[i], l[j + 1])]
+# l = list(map(int, input().split()))
+# subset = {}
+# for i in range(len(l)):
+#     for j in range(i, len(l) - 1):
+#         s = l[i] + l[j + 1]
+#         if s in subset:
+#             subset.get(s).append((l[i], l[j + 1]))
+#         else:
+#             subset[s] = [(l[i], l[j + 1])]
 
-key_min = min(subset.keys(), key=(lambda k: subset[k]))
-print(len(subset[key_min]))
+# key_min = min(subset.keys(), key=(lambda k: subset[k]))
+# print(len(subset[key_min]))
 # 1 2 3 4
 # 0
 # 1,1
@@ -20,3 +20,27 @@ print(len(subset[key_min]))
 # 1
 # 2,3
 # 3,4
+
+conjunto = [1, 2, 3, 4]
+k = 2
+lista = []
+iteraciones = [0]
+
+
+def subconjuntos(l, k):
+    if k == len(l):
+        if not l in lista:
+            lista.append(l)
+        return
+    for i in l:
+        aux = l[:]
+        aux.remove(i)
+        result = subconjuntos(aux, k)
+        iteraciones[0] += 1
+        if not result in lista and result:
+            lista.append(result)
+
+
+subconjuntos(conjunto, k)
+print(lista)
+
