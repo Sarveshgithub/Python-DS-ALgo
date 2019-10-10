775807144476579da6ea08f1abf243d43117595a
sarvesh574 <sarvesh.kumar@silverlinecrm.com>
2019-10-10 11:59:43 +0530

    complete find smallest element

--- DS/Array/findsmallestElemetInrotated.py
+++ DS/Array/findsmallestElemetInrotated.py
@@ -1,15 +1,20 @@
 def bs(a, tempArray, L, R):
     mid = (L + R) // 2
-    if tempArray[mid + 1] == "T":
+    if tempArray[mid] == "T" and tempArray[mid + 1] == "F":
+        return a[mid + 1]
+    elif tempArray[mid + 1] == "T":
         L = mid + 1
+        return bs(a, tempArray, L, R)
     elif tempArray[mid - 1] == "F":
         R = mid - 1
+        return bs(a, tempArray, L, R)
     elif tempArray[mid] == "F":
         return a[mid]
-    return bs(a, tempArray, L, R)
+    else:
+        return -1
 
 
-a = [3, 4, 5, 6, 7, 1, 2]
+a = [3, 1, 2]
 # T, T, T, T, T, F,F
 end = a[len(a) - 1]
 tempArray = []
@@ -20,6 +25,10 @@ for i in a:
         tempArray.append("T")
 
 L, R = 0, len(a) - 1
-smallest = bs(a, tempArray, L, R)
+smallest = -1
+if "T" not in tempArray:
+    smallest = a[0]
+else:
+    smallest = bs(a, tempArray, L, R)
 print(smallest)
 
