b961ecb98d0deb25dd2b514283934520c2f7072d
sarvesh574 <sarvesh.kumar@silverlinecrm.com>
2019-10-10 19:44:03 +0530

    added

--- DS/Array/search-an-element-in-a-sorted-and-pivoted-array.py
+++ DS/Array/search-an-element-in-a-sorted-and-pivoted-array.py
@@ -1,19 +1,38 @@
-def searchElement(arr, e):
-    L, R = 0, len(arr) - 1
-    while arr[L] <= arr[R]:
-        mid = L + R // 2
+# binary search
+def bs(arr, L, R, e):
+    print(arr, L, R, e)
+    while L <= R:
+        mid = (L + R) // 2
+        print("before", L, R, mid)
+        print(arr[L], arr[R])
         if arr[mid] == e:
             return mid
         elif arr[mid] < e:
             L = mid + 1
         elif arr[mid] > e:
             R = mid - 1
-
+        print("After", L, R, mid)
     return -1
 
 
-a = [30, 40, 50, 10, 20]
-e = 10
-a.sort()
-print(searchElement(a, e))
+def findPivot(a, l, r):
+    if l == r:
+        return l
+    mid = (l + r) // 2
+    print(mid)
+    if a[mid] > a[mid + 1]:
+        return mid + 1
+    if a[mid - 1] < a[mid]:
+        l = mid + 1
+    else:
+        r = mid - 1
+    return findPivot(a, l, r)
+
+
+a = [11, 15, 26, 38, 9, 10]
+pivot = -1
+l, r = 0, len(a) - 1
+
+pivot = findPivot(a, l, r)
+# print(a[pivot:])
 
