74acb786f92b76e5d1f23d515a14dd0e380bd139
sarvesh574 <sarvesh.kumar@silverlinecrm.com>
2019-10-11 19:56:31 +0530

    Done search element in a sorted revered order array

--- DS/Array/search-an-element-in-a-sorted-and-pivoted-array.py
+++ DS/Array/search-an-element-in-a-sorted-and-pivoted-array.py
@@ -1,6 +1,5 @@
 # binary search
 def bs(arr, L, R, e):
-    print(arr, L, R, e)
     while L <= R:
         mid = (L + R) // 2
         print("before", L, R, mid)
@@ -16,23 +15,30 @@ def bs(arr, L, R, e):
 
 
 def findPivot(a, l, r):
-    if l == r:
-        return l
     mid = (l + r) // 2
-    print(mid)
+    print("m:", mid, "l:", l, "r:", r)
+    if l == r:
+        return r + 1
     if a[mid] > a[mid + 1]:
         return mid + 1
-    if a[mid - 1] < a[mid]:
+    if a[mid - 1] <= a[mid]:
         l = mid + 1
     else:
         r = mid - 1
     return findPivot(a, l, r)
 
 
-a = [11, 15, 26, 38, 9, 10]
+a = [34, 43, 45, 1, 2, 3, 4, 5]
 pivot = -1
 l, r = 0, len(a) - 1
 
 pivot = findPivot(a, l, r)
+target = 4
+if target == a[pivot]:
+    print(pivot)
+elif target < a[pivot] or target > a[len(a) - 1]:
+    print(bs(a, l, pivot - 1, target))
+else:
+    print(bs(a, pivot, r, target))
 # print(a[pivot:])
 
