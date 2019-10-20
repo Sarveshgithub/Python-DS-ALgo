b234cab23c5959a298b9a1d1e47548d4d632182e
sarvesh574 <sarvesh.kumar@silverlinecrm.com>
2019-10-21 00:14:02 +0530

    add new hackerank problem

---
+++ HackerRank/dynamic-array.py
@@ -0,0 +1,11 @@
+i1 = list(map(int, input().split(" ")))
+n, q = i1[0], i1[1]
+seq = [[] for i in range(n)]
+quary = []
+for _ in range(q):
+    quary.append(list(map(int, input().split(" "))))
+
+print(seq)
+print(quary)
+
+# q1
