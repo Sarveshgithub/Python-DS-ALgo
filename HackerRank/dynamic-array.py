0de3fdebb09ad1bdeeee83d2b289b6e5f4e52591
sarvesh574 <sarvesh.kumar@silverlinecrm.com>
2019-10-21 18:49:28 +0530

    commit

--- HackerRank/dynamic-array.py
+++ HackerRank/dynamic-array.py
@@ -1,11 +1,25 @@
+def sequence(seqList, quary, n):
+    lastAnswer = 0
+    for i in range(len(quary)):
+
+        q, x, y = quary[i][0], quary[i][1], quary[i][2]
+        # q1
+        if q == 1:
+            seq = (x ^ lastAnswer) % n
+            seqList[seq].append(y)
+            print(seqList)
+        elif q == 2:
+            seq = (x ^ lastAnswer) % n
+            print(seq)
+            lastAnswer = y % len(seqList[seq])
+            print(seqList[seq][lastAnswer], seqList)
+
+
 i1 = list(map(int, input().split(" ")))
 n, q = i1[0], i1[1]
-seq = [[] for i in range(n)]
+seqList = [[] for i in range(n)]
 quary = []
 for _ in range(q):
     quary.append(list(map(int, input().split(" "))))
 
-print(seq)
-print(quary)
-
-# q1
+sequence(seqList, quary, n)
