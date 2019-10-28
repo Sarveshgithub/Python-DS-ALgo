f317db0398c7821103692418796e1299691c9df2
sarvesh574 <sarvesh.kumar@silverlinecrm.com>
2019-10-29 01:09:46 +0530

    solve using prefix algo

--- HackerRank/Array_Manipulation.py
+++ HackerRank/Array_Manipulation.py
@@ -2,12 +2,14 @@
     # 1 5 3
     # 4 8 7
     # 6 9 1
+    
 i1 = list(map(int,input().split(" ")))
 n,m = i1[0],i1[1]
-a = [0 for i in range(n)]
+a = [0]*(n+2)
 for i in range(m):
     val = list(map(int,input().split(" ")))
-    for i in range(val[0]-1,val[1]):
-        a[i] += val[2]
-
+    a[val[0]]+= val[2]
+    a[val[1]+1]+= -val[2]
+for i in range(1,len(a)):
+    a[i] = a[i-1]+a[i]
 print(max(a))
